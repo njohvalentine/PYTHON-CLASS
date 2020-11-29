2582e512a92b1e796eb641c9a6654638f2a3497a
valentine <valentine@gmail.com>
2020-11-29 11:40:11 -0500

    operators and print formating

---
+++ 2operator.py
@@ -0,0 +1,39 @@
+# this is for exponential operation
+result=2**2
+print(result)
+print("the value of the operation is ", result)
+
+# for multiplication operations
+result=2*3
+print(result)
+print("the value of the operation is ", result)
+
+#for division operation
+result=9/2
+print(result)
+print("the value of the operation is ", result)
+
+# for interger division or floored quotion
+result=9//2
+print(result)
+print("the value of the operation is ", result)
+
+# for interger addition operation
+result=5+2
+print(result)
+print("the value of the operation is ", result)
+
+# for modulus operation
+result=9%2
+print(result)
+print("the value of the operation is ", result)
+
+# hard coding our variable values to make them resuable
+#a=9
+#b=3
+a=input("print first value: ")
+b=input("print second value: ")
+c=int(a)+int(b)
+print(c)
+
+
---
+++ 3pythonformat.py
@@ -0,0 +1,5 @@
+first_name= 'vally'
+last_name='njoh'
+print (first_name + ' ' + last_name)
+print ("{} {}".format(first_name, last_name))
+print (f' {first_name} {last_name}')
