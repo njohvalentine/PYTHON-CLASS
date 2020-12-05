6bf56f430d149b19a18cb03dd965e89355d7f53e
valentine <valentine@gmail.com>
2020-12-05 13:14:50 -0500

    cal

---
+++ example.py
@@ -0,0 +1,4 @@
+first_name='valentine'
+last_name='njoh'
+#my-name=first_name + last_name
+print(first_name +  last_name)
---
+++ strings.py
@@ -0,0 +1,31 @@
+f=7.5
+g=int(f)
+print(g)
+print(type(g))
+
+
+string="class"
+print(string[1:3])
+
+
+string= 'hallo'
+print(string.startswith('h'))
+
+clovis=len(string)
+print(clovis)
+
+string='  vally  '
+print(string.rstrip())
+
+
+
+
+
+
+
+
+
+
+
+
+
