37eacc908a3054c9fbda6498c60634f4fb4adc17
valentine <valentine@gmail.com>
2020-12-06 11:18:42 -0500

    if,list and collection

---
+++ age_cal.py
@@ -0,0 +1,3 @@
+birth_year= input("In what year were you born? ")
+age=2020 - int(birth_year)
+print(age)
---
+++ if_condition.py
@@ -0,0 +1,6 @@
+environment=input('what env are you working')
+if environment == 'prod':
+    print('You have access to this environment')
+else: 
+    print('You are not permited to work here')
+    
