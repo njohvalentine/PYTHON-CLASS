b18d660f8ce504045804d822a9ae12ebe6f380f8
valentine <valentine@gmail.com>
2020-12-06 10:11:17 -0500

    assignment modified

--- assignment.py
+++ assignment.py
@@ -1,6 +1,5 @@
 # writing a code to promt students to fill in their name and marks and have the code print it in caps.
 student_name=input('student_name: ')
-call_student=str.upper(student_name)
-student_marks=int(input('student_marks: '))
-print(("my name is " "{} i scored {}" "in my python exams".format(call_student, student_marks)))
+student_marks=int(input("my marks"))
+print('my name is {} and i scored {} in my python exams' .format(student_name.upper(),student_marks))
 
