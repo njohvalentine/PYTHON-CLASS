e6697e8b16d1022444a7b20fa305d32561a9a381
valentine <valentine@gmail.com>
2020-12-06 08:18:01 -0500

    my first python code

---
+++ assignment.py
@@ -0,0 +1,6 @@
+# writing a code to promt students to fill in their name and marks and have the code print it in caps.
+student_name=input('student_name')
+call_student=str.upper(student_name)
+student_marks=int(input('student_marks'))
+print(("my name is" "{} i scored {}" "in my python exams".format(call_student, student_marks)))
+
